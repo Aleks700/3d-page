aaabfd92972566d0c710736a6afbf985fb380cc1
Alekstok97 <alekstok97@mail.ru>
2022-07-12 14:05:44 +0600

    replace children

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
-import * as fmViewer from './viewer.js';
+import * as fmViewer from "./viewer.js";
 
+console.log("started");
 
-
-
-
-
-console.log('started')
-
-const SCAN_WIDTH = '400px';
-const SCAN_HEIGHT = '400px';
+const SCAN_WIDTH = "400px";
+const SCAN_HEIGHT = "400px";
 
 const SCANS = [
   {
-    id: 'backpack',
-    file: 'backpack.fm'
+    id: "backpack",
+    file: "backpack.fm",
   },
   {
-    id: 'boots',
-    file: 'boots.fm'
+    id: "boots",
+    file: "boots.fm",
   },
   {
-    id: 'bag',
-    file: 'bag.fm'
+    id: "bag",
+    file: "bag.fm",
   },
   {
-    id: 'puppet',
-    file: 'puppet.fm'
-  }
+    id: "puppet",
+    file: "puppet.fm",
+  },
 ];
 
-document.addEventListener('DOMContentLoaded', async event => {
-
-
+document.addEventListener("DOMContentLoaded", async (event) => {
   // console.log(carousel);
-
-
-
-
-
 
   await fmViewer.default();
 
-
-
   // for (let scan of SCANS) {
-    let canvas = document.createElement('canvas');
-    const block = document.getElementById('canvasBlock');
-    console.log(block);
-    block.appendChild(canvas);
-    canvas.id = 'scan.id'
+  let canvas = document.createElement("canvas");
+  const block = document.getElementById("canvasBlock");
+  console.log(block);
+  block.appendChild(canvas);
+  canvas.id = "scan.id";
 
-    canvas.setAttribute('height', SCAN_HEIGHT);
-    canvas.setAttribute('width', SCAN_WIDTH);
-    // canvas.setStyle('height:100%,width:100%')
+  canvas.setAttribute("height", SCAN_HEIGHT);
+  canvas.setAttribute("width", SCAN_WIDTH);
+  // canvas.setStyle('height:100%,width:100%')
 
+  let viewer = fmViewer.Viewer.create(canvas);
+
+  let resp = await fetch("/scan/" + "bag.fm");
+  if (!resp.ok) {
+    throw "failed to fetch a model ";
+  }
+
+  let buf = await resp.arrayBuffer();
+  console.log(resp.arrayBuffer.length);
+  console.log(buf);
+  await viewer.loadFmBuffer(buf);
+
+  await viewer.renderAll();
+
+  const carousel = document.querySelector(".carousel");
+  console.log(carousel.childNodes);
+  carousel.addEventListener("click", async (event) => {
+    const block = document.getElementById("canvasBlock");
+    let canvas = document.createElement("canvas");
+    block.replaceChildren(canvas);
     let viewer = fmViewer.Viewer.create(canvas);
 
-    let resp = await fetch('/scan/' + 'bag.fm');
+    let resp = await fetch("/scan/" + "boots.fm");
     if (!resp.ok) {
-      throw 'failed to fetch a model ';
+      throw "failed to fetch a model ";
     }
-   
+
     let buf = await resp.arrayBuffer();
     console.log(resp.arrayBuffer.length);
     console.log(buf);
@@ -68,32 +74,8 @@ document.addEventListener('DOMContentLoaded', async event => {
 
     await viewer.renderAll();
 
+    // const clicked = event.currentTarget
 
-
-
-    const carousel = document.querySelector('.carousel');
-    console.log(carousel.childNodes);
-    carousel.addEventListener('click', async (event) =>{
-      let viewer = fmViewer.Viewer.create(canvas);
-
-      let resp = await fetch('/scan/' + 'boots.fm');
-      if (!resp.ok) {
-        throw 'failed to fetch a model ';
-      }
-     
-      let buf = await resp.arrayBuffer();
-      console.log(resp.arrayBuffer.length);
-      console.log(buf);
-      await viewer.loadFmBuffer(buf);
-  
-      await viewer.renderAll();
-
-      // const clicked = event.currentTarget
-      
-      console.log(event.currentTarget); 
-    })
-
-
-
-  // }
+    console.log(event.currentTarget);
+  });
 });

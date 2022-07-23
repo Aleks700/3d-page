8d42d60ac3397b2501c86759c85deff763754bad
Alex <alekstok97@mail.ru>
2022-07-24 03:35:45 +0600

    working php

--- mail.php
+++ mail.php
@@ -1,16 +1,26 @@
 
 <?php 
+
+
+$email = $_POST['email'];
+
+
+
+if($email!=NULL){
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$massage = "php worsted langaigh in the world".$name;
+$massage = "message from".$name."email was".$email;
 $to = 'alekstok97@mail.ru';
 $subject = 'message from work';
-$sending_message = "message from".$name.''.$message;
-//header("Location: https://www.google.com");
+$sending_message = "message from ".$name.'your email'.$email;
+
 
 $headers = "From: noreply@yoursite.com"."\r\n"."CC:".$name;
+
 print mail($to, $subject,$massage);
-echo $name;
+
+}
 
 ?>
